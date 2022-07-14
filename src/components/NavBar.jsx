436bcab244580a98b89e52345ec1cb8324ae0472
LeoSolari <95233443+LeoSolari@users.noreply.github.com>
2022-07-14 14:46:59 -0300

    Revert "Segundo commit"

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,10 @@ const NavBar = () => {
     <div>
       <ul>
         <li>
-          <NavLink to="/main">-Main-</NavLink>
-          <NavLink to="/">-Login-</NavLink>
-          <NavLink to="/new-user">-Nuevo Usuario-</NavLink>
-          <NavLink to="*">-404-</NavLink>
-          <NavLink to="/pacientes">-PACIENTES-</NavLink>
+          <NavLink to="/main">Main</NavLink>
+          <NavLink to="/">Login</NavLink>
+          <NavLink to="/new-user">Nuevo Usuario</NavLink>
+          <NavLink to="*">404</NavLink>
         </li>
       </ul>
     </div>
